68ab923af844562d7a23bfabb41777c902893f80
Andrei Filyaev <soheevich@gmail.com>
2019-06-30 18:49:13 +0500

    Add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const _DEV_ = process.env.NODE_ENV !== "production";
+const PORT = 3000;
 
 module.exports = {
 	entry: './src/index.js',
+	mode: _DEV_ ? 'development' : 'production',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	devtool: _DEV_ ? 'source-map' : 'eval',
 	module: {
 		rules: [
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader"
-				},
+				loader: "babel-loader",
 			},
 			{
 				test: /\.css$/,
-				use: ["style-loader", "css-loader"]
+				use: ["style-loader", "css-loader"],
 			}
 		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./src/index.html"
-		})
-	]
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+		}),
+	],
+	devServer: {
+		port: PORT,
+	}
 };
