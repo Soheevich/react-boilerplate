4f35375ec3bff6beb5723012d43b54cd98b622cd
Andrei Filyaev <soheevich@gmail.com>
2019-07-02 22:19:26 +0500

    add redux

--- src/customRedux/reducer.js
+++ src/customRedux/reducer.js
@@ -1,3 +1,5 @@
+const {createStore} = require('redux');
+
 function reducer(state, action) {
 	if (action.type === "INCREMENT") {
 		return {count: state.count + action.amount};
@@ -8,34 +10,13 @@ function reducer(state, action) {
 	}
 }
 
-function createStore(reducer, state) {
-	let _reducer = reducer;
-	let _state = state;
-	let _callbacks = [];
-
-	function getState() {
-		return _state;
-	}
-
-	function dispatch(action) {
-		_state = _reducer(_state, action);
-		_callbacks.forEach(callback => callback());
-	}
-
-	function subscribe(callback) {
-		_callbacks.push(callback);
-		return () => _callbacks = _callbacks.filter(cb => cb !== callback);
-	}
-
-	return {getState, dispatch, subscribe};
-}
-
 const initialState = {count: 0};
 
 const store = createStore(reducer, initialState);
 
 const incrementAction = {type: "INCREMENT", amount: 5};
 const decrementAction = {type: "DECREMENT", amount: 3};
+const nothingAction = {type: ""};
 
 const unsubscribe = store.subscribe(() => console.log("State changed one", store.getState().count));
 store.subscribe(() => console.log("State changed two", store.getState().count));
@@ -43,4 +24,4 @@ store.subscribe(() => console.log("State changed two", store.getState().count));
 store.dispatch(incrementAction);
 unsubscribe();
 store.dispatch(decrementAction);
-store.dispatch({});
+store.dispatch(nothingAction);
